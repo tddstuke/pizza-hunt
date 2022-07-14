0342f96916c503e42815bce6251d6336d22540ce
Todd Stuke <tddstuke@gmail.com>
2022-07-13 19:57:48 -0500

    added validators

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -26,7 +26,7 @@ const commentController = {
     Comment.findOneAndUpdate(
       { _id: params.commentId },
       { $push: { replies: body } },
-      { new: true }
+      { new: true, runValidators: true }
     ).then((dbPizzaData) => {
       if (!dbPizzaData) {
         res.status(404).json({ message: "No pizza found with this id!" });
